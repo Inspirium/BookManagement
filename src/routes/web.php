9b9402cab6034acddd365f7411f8fc932627f186
Marko Banusic <mbanusic@gmail.com>
2017-06-09 22:13:20 +0200

    Rename some routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,19 +1,21 @@
 <?php
 
-Route::group(['namespace' => 'Inspirium\BookManagement\Controllers', 'middleware' => 'web', 'prefix' => 'book'], function() {
+Route::group(['namespace' => 'Inspirium\BookManagement\Controllers', 'middleware' => 'web', 'prefix' => 'books'], function() {
 
     Route::get('/', 'BookController@showBooks');
-    Route::get('show/{id}', 'BookController@showBook');
-    Route::get('edit/{id?}', 'BookController@editBook');
+    Route::group(['prefix' => 'book'], function() {
+	    Route::get('show/{id}', 'BookController@showBook');
+	    Route::get('edit/{id?}', 'BookController@editBook');
+    });
 
+	Route::get('authors', 'AuthorController@showAuthors');
     Route::group(['prefix' => 'author'], function() {
-        Route::get('/', 'AuthorController@showAuthors');
         Route::get('show/{id}', 'AuthorController@showAuthor');
         Route::get('edit/{id?}', 'AuthorController@editAuthor');
     });
 
+	Route::get('categories', 'CategoryController@showCategories');
     Route::group(['prefix' => 'category'], function() {
-        Route::get('/', 'CategoryController@showCategories');
         Route::get('show/{id}', 'CategoryController@showCategory');
         Route::get('edit/{id?}', 'CategoryController@editCategory');
     });
